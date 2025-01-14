082825adc11490bf75ddf6a8898c6f1b48e39325
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-01-14 17:01:38 +0000

    commit

--- DocSmart/DocSmart/settings.py
+++ DocSmart/DocSmart/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,11 +52,12 @@ MIDDLEWARE = [
 ROOT_URLCONF = "DocSmart.urls"
 
 import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Ensure this points to the templates folder
+        'DIRS': [os.path.join(BASE_DIR, "templates")],  # Ensure this points to the templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
