eca1c3deae75ea3d7519733d84ecebbb769c6879
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-01-16 20:03:05 +0000

    commit

--- DocSmart/core/urls.py
+++ DocSmart/core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('services/', views.services, name='services'),
     path('', views.home, name='home'),
+
 ]
